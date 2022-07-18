13ccc7bf557076e5d594e776673b5d9250063432
Kawser365 <kawser07.khk@gmail.com>
2022-07-18 12:46:36 +0600

    some Update

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+// const express = require('express')
+// const bodyParser = require('body-parser');
+// const cors = require ('cors');
+// const fileUpload = require('express-fileupload')
+// const MongoClient = require('mongodb').MongoClient;
+// const ObjectID = require('mongodb').ObjectID;
+// const publicRouter = require('./publicRouter/publicRouter');
+// const app = require('./adminRouter/adminRouter');
+// require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser');
-const cors = require ('cors');
+const cors = require ('cors')
 const fileUpload = require('express-fileupload')
 const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const publicRouter = require('./publicRouter/publicRouter');
-const adminRouter = require('./adminRouter/adminRouter');
+const ObjectId = require("mongodb").ObjectId;
+const admin = require("firebase-admin");
+const fs = require('fs-extra')
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 4000;
 
@@ -17,57 +27,253 @@ app.use(express.static('foods'));
 app.use(fileUpload());
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqegz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// client.connect(err => {
-//   console.log(err)
-//   const foodsCollection = client.db("foodsData").collection("foodCollections");
-//   console.log('database connection successfully')
-//   client.close();
-// });
-
-
-app.use('/', publicRouter)
-app.use('/', adminRouter)
-
-// adminRouter.post('/addFood', (req, res)=>{
-//   const name = req.body.name;
-//   const price = req.body.price;
-//   const file = req.files.file;
-//   console.log(name,price,file)
-//   const filePath = `${__dirname}/foods/${file.name}`;
-//   file.mv(filePath, err =>{
-//       if(err){
-//           console.log(err)
-//           return res.status(500).send({msg:'faield to upload msg'})
-//       }
-//       const newImg = fs.readFileSync(filePath);
-//       const encImg = newImg.toString('base64');
-//       var image = {
-//           contentType: req.files.file.mimetype,
-//           size: req.files.file.size,
-//           img: Buffer(encImg, 'base64')
-//       }
-//       foodsCollection.insertOne({name, price, image})
-//       .then(result=>{
-//           fs.remove(filePath, err =>{
-//               if(err){
-//                   console.log(err)
-//               }
-//               res.send(result.insertedCount > 0)
-//           })
-         
-//       })
-//       // return res.send({name: file.name, path: `/${file.name}`})
-//   })
-
-// })
-
-
-
-
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwobc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+
+
+
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+      const token = req.headers.authorization.split(' ')[1];
+
+      try {
+          const decodedUser = await admin.auth().verifyIdToken(token);
+          req.decodedEmail = decodedUser.email;
+      }
+      catch {
+
+      }
+
+  }
+  next();
+}
+
+
+client.connect(err => {
+  const foodsCollection = client.db(`${process.env.DB_NAME}`).collection("foodCollections");
+  const orderCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
+  const usersCollection = client.db(`${process.env.DB_NAME}`).collection("users");
+  console.log('database connection successfully')
+  // client.close();
+
+  app.post('/addFood', (req, res)=>{
+    const name = req.body.name;
+    const weight = req.body.weight;
+    const price = req.body.price;
+    const file = req.files.file;
+    const newImg = file.data;
+    const encImg = newImg.toString('base64');
+    var image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    };
+        foodsCollection.insertOne({ name, weight, price, image })
+        .then(result=>{
+                res.send(result.insertedCount > 0)
+        })
+        console.log(req.body)
+    })
+
+    app.get('/allProduct', (req,res)=>{
+      // const search = req.query.search;
+      // {name: {$regex: search}}
+        foodsCollection.find()
+        .toArray((err, documents)=>{
+            res.send(documents)
+            
+        })
+
+})
+
+   app.delete('/allProduct/:id', (req,res)=>{
+    foodsCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then(result=>{
+      console.log(result)
+    })
+
+})
+
+
+   app.get('/allProduct/:id', (req,res)=>{
+    foodsCollection.find({_id: ObjectId(req.params.id)})
+    .toArray((err, documents) => {
+     res.send(documents)
+    })
+   })
+
+
+   app.patch('/allProduct/:id', (req,res)=>{
+    foodsCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set:{name: req.body.name, price: req.body.price, weight: req.body.weight}
+    })
+    .then(result=>{
+      console.log(result)
+    })
+
+})
+
+
+app.post('/order',(req,res)=>{
+  const newOrder = req.body;
+  orderCollection.insertOne(newOrder)
+  .then(result=>{
+      res.send(result.insertedId)
+  })
+  console.log(req.body)
+  
+})
+
+// app.get('/order',(req,res)=>{
+//   orderCollection.find({email:req.query.email})
+//   .toArray((err, order)=>{
+//       res.send(order)
+//   })
+// })
+
+app.get('/myOrder/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await orderCollection.findOne(query);
+  res.json(result);
+})
+
+app.put('/myOrder/:id', (req, res) => {
+  const id = req.params.id;
+  const payment = req.body;
+  const filter = { _id: ObjectId(id) };
+  const updateDoc = {
+      $set: {
+          payment: payment
+      }
+  };
+  const result = orderCollection.updateOne(filter, updateDoc);
+  res.json(result);
+});
+
+
+app.get('/myOrder', (req,res)=>{
+  // console.log(req.query.email)
+  orderCollection.find({email:req.query.email})
+  .toArray((err, products)=>{
+    res.send(products)
+    console.log(products)
+  })
+})
+
+// delete order
+app.delete('/myOrder/:id', (req,res)=>{
+  // console.log(req.query.email)
+  orderCollection.deleteOne({_id: ObjectId(req.params.id)})
+  .then(result=>{
+    console.log(result)
+  })
+})
+
+
+
+app.post('/create-payment-intent', async (req, res) => {
+  const price = req?.body?.price
+  console.log(price)
+  const amount = price * 100
+  if (amount > 999999) {
+      return res.status(500).send({ message: 'Your price is too high' })
+  }
+  if(price){
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      payment_method_types: [
+          "card"
+      ],
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+});
+  }
+  
+
+  
+
+})
+
+ // use post to get products by ids
+ app.post('/productByIds', (req, res) =>{
+  const keys = req.body;
+  const ids = keys.map(id => ObjectId(id));
+  const query = {_id: {$in: ids}}
+  const cursor = foodsCollection.find(query);
+  const products = cursor.toArray();
+  console.log(keys);
+  res.send(products);
+})
+
+// usres
+
+app.get('/users/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+  let isAdmin = false;
+  if (user?.role === 'admin') {
+      isAdmin = true;
+  }
+  res.json({ admin: isAdmin });
+})
+
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  console.log(result);
+  res.json(result);
+});
+
+app.put('/users', async (req, res) => {
+  const user = req.body;
+  const filter = { email: user.email };
+  const options = { upsert: true };
+  const updateDoc = { $set: user };
+  const result = await usersCollection.updateOne(filter, updateDoc, options);
+  res.json(result);
+});
+
+app.put('/users/admin', verifyToken, async (req, res) => {
+  const user = req.body;
+  const requester = req.decodedEmail;
+  if (requester) {
+      const requesterAccount = await usersCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: 'admin' } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+      }
+  }
+  else {
+      res.status(403).json({ message: 'you do not have access to make admin' })
+  }
+
+})
+
+
+
+
+
+});
