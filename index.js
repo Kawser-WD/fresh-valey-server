481e41dd204d9839af51859a50d8a266f48cf09c
Kawser365 <kawser07.khk@gmail.com>
2022-08-26 22:33:32 +0600

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({credentials: true, origin: "https://fresh-food-ecommerce.web.app/"}));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static('foods'));
